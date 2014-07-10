42cc72061537977e64490c807c3525c15a997cba
luhaishan <l00172010@126.com>
2014-07-09 23:13:41 -0700

    initialize the playground and welcome message.

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 snake: main.o
-	gcc -o snake main.o
+	gcc -o snake main.o -lcurses
 
 main.o: main.c
 	gcc -c main.c
+
+clean:
+	-rm snake main.o
--- main.c
+++ main.c
@@ -4,7 +4,15 @@
 */
 
 #include <stdio.h>
+#include <curses.h>
 
+void drawHorTopLine(int x, int y, int width);//draw the top horizon line
+void drawHorBtmLine(int x, int y, int width);//draw the bottom horizon line
+void drawLeftVerLine(int x, int y, int height);//draw the left vertical line
+void drawRightVerLine(int x, int y, int height);//draw the right vertical line
+
+void initWelcomeMsg();
+void initPlayground();
 void initGame();
 void runGame();
 void saveGame();
@@ -21,9 +29,16 @@ int main(int argc, char* argv[])
 void initGame()
 {
 	printf("Init the Game.\n");
-	printf("-- Drawing a User Interface.\n");
+	//printf("-- Drawing a User Interface.\n");
+	initscr();
+	initPlayground();
+    initWelcomeMsg();
 	printf("-- There's a handler to handle user input\n");
 	printf("-- Press the space button to start the Game.\n");
+    refresh();
+    getch();
+    endwin();
+
 }
 
 void runGame()
@@ -38,4 +53,58 @@ void saveGame()
 {
 	printf("The Game is exiting, do some save stuff.\n");
 	printf("-- At the very beginning , we may save nothing.\n");
+}
+
+void initPlayground()
+{
+	drawHorTopLine(0, 0, 80);
+	drawHorBtmLine(39, 0, 80);
+	drawLeftVerLine(0, 0, 40);
+	drawRightVerLine(0, 79, 40);
+}
+
+void drawHorTopLine(int x, int y, int width)
+{
+	move(x, y);
+	int i = 0;
+	while (i < width) {
+		printw("x");
+		i++;
+	}
+}
+
+void drawHorBtmLine(int x, int y, int width)
+{
+    move(x, y);
+    int i = 0;
+    while (i < width) {
+        printw("x");
+        i++;
+    }
+}
+
+void drawLeftVerLine(int x, int y, int height)
+{
+    int i = 0;
+    while (i < height) {
+		move(x+i, y);
+        printw("x");
+        i++;
+    }
+}
+
+void drawRightVerLine(int x, int y , int height)
+{
+    int i = 0;
+    while (i < height) {
+		move(x+i, y);
+        printw("x");
+        i++;
+    }
+}
+
+void initWelcomeMsg()
+{
+	move(5, 30);
+	printw("Press space to start!");
 }
